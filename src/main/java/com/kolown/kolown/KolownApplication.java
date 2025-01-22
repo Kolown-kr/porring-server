346bb59dce02cf79df1dc6eb8eacdf4f7abc112f
seongha <11pi885@gmail.com>
2025-01-22 21:52:11 +0900

    chore: init project

---
+++ src/main/java/com/kolown/kolown/KolownApplication.java
@@ -0,0 +1,13 @@
+package com.kolown.kolown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kolown/kolown/KolownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolown.kolown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
