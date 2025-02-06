dc0d82ee53fe48b8cb1a20ee7b0512860debd0d2
Chanwoo Kim <kcw2205@naver.com>
2025-02-06 20:48:05 +0900

    refactor: 💡 Test Main 클래스 이름 변경

---
+++ src/test/java/com/kolown/kolown/PorringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolown.kolown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
