599ad1b0f9b77e2d4fdc09cb310e035c61aba255
Chanwoo Kim <kcw2205@naver.com>
2025-02-17 21:50:54 +0900

    feat: 🎸 Security Config 공개 정보 업데이트
    boards api 추가

--- src/main/java/com/kolown/porring/security/SecurityConfig.java
+++ src/main/java/com/kolown/porring/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/accounts/join").permitAll()
                         .requestMatchers("/accounts/login").permitAll()
+                        .requestMatchers("/boards/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(
