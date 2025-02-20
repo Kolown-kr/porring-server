f7e6146001d64fd50d733250f48e64b051b3c978
Chanwoo Kim <kcw2205@naver.com>
2025-02-20 15:17:57 +0900

    refactor: 💡 Reaction 검색 코드 Jpa Method로 간소화

--- src/main/java/com/kolown/porring/board/repository/ReactionRepository.java
+++ src/main/java/com/kolown/porring/board/repository/ReactionRepository.java
@@ -3,7 +3,6 @@ package com.kolown.porring.board.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +10,5 @@ import com.kolown.porring.board.entity.Reaction;
 
 @Repository
 public interface ReactionRepository extends JpaRepository<Reaction, Reaction.ReactionId> {
-    @Query("SELECT r FROM Reaction r WHERE r.id.boardId = :boardId")
-    List<Reaction> findByBoardId(@Param("boardId") Long boardId);
+    List<Reaction> findByIdBoardId(@Param("boardId") Long boardId);
 }
